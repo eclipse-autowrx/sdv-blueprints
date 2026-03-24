1c99e73f0ed030f4f7bf3bd660e5c055d768b331
Goutham <gxc2kor@bosch.com>
2026-03-24 16:47:40 +0530

    new fork

---
+++ matlab-connector/starter-kit/wipersCtrlrPrj/src/da_connector.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2025 Eclipse Foundation.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: MIT
+#ifndef MY_SCALE_H
+
+#define MY_SCALE_H
+
+double da_connector();
+
+#endif
