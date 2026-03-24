427fe096cfa5942e0dc23996bbd814bad99d76cf
Goutham <gxc2kor@bosch.com>
2026-03-24 14:14:33 +0530

    Changed license to Eclipse

--- matlab-connector/starter-kit/wipersCtrlrPrj/scripts/initExtMode.m
+++ matlab-connector/starter-kit/wipersCtrlrPrj/scripts/initExtMode.m
@@ -1,12 +1,11 @@
-/**
- * Copyright (c) 2025 Robert Bosch GmbH.
- *
- * This program and the accompanying materials are made available under the
- * terms of the MIT License which is available at
- * https://opensource.org/licenses/MIT.
- *
- * SPDX-License-Identifier: MIT
- */
+// Copyright (c) 2025 Eclipse Foundation.
+// 
+// This program and the accompanying materials are made available under the
+// terms of the MIT License which is available at
+// https://opensource.org/licenses/MIT.
+//
+// SPDX-License-Identifier: MIT
+
  % this script registers the target configuration for external mode
 fprintf("\n### Info: Initialize target configuration...\n");
 % load configuration
